a30bb7c8c016f4b3ddd5d1f5093ac0c4091692fb
Siroibma <62669274+Siroibma@users.noreply.github.com>
2021-05-20 12:19:55 -0400

    latest commit

--- js/api.js
+++ js/api.js
@@ -1,13 +1,85 @@
+
+
+
 let app = new Vue({
   el: '#app',
   data: {
     welcomeMessage: 'Welcome To My Api Page',
     results: [],
-    total_number: 6
+    total_number: 6,
+    output: ''
   },
   mounted() {
-    axios.get('http://206.189.202.188:2523/articles/index.json').then(response => {
-      this.results = response.data
-    })
-  }
+
+    axios ({
+      method: 'get',
+      url: 'http://206.189.202.188:2523/articles/index.json',
+      params : {
+        _limit: 6
+      }
+      })
+      .then(response => {
+        this.results = response.data
+        console.log(this.results)
+      })
+  },
+  methods: {
+    formSubmit(e) {
+        e.preventDefault();
+        let currentObj = this;
+        this.axios.post('http://206.189.202.188:2523/api/users/add', {
+            email: 'ambioris42432@gmail.com',
+            password: 'test'
+        })
+    }
+},
 });
+
+function addUser() {
+  axios
+    .post('http://206.189.202.188:2523/api/users/add', {
+      email: 'ambioris42432@gmail.com',
+      password: 'test'
+    })
+    .then(res => console.log(res.request.response.data[0]))
+    .catch(err => console.error(err));
+}
+
+function deleteArticle() {
+  axios
+    .post('http://206.189.202.188:2523/api/users/add', {
+      email: 'ambioris42432@gmail.com',
+      password: 'test'
+    })
+    .then(res => console.log(res.request.response.data[0]))
+    .catch(err => console.error(err));
+}
+
+
+function createArticle() {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken'
+    }
+  };
+
+  axios
+    .post(
+      'https://jsonplaceholder.typicode.com/todos',
+      {
+        title: 'New Todo',
+        completed: false
+      },
+      config
+    )
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
+}
+
+
+
+
+document.getElementById('post').addEventListener('click', addUser);
+document.getElementById('create').addEventListener('click', createArticle);
+document.getElementById('delete').addEventListener('click', deleteArticle);
