78c8ec3ac647557c56a5daa5af87aaee27e8ca08
Siroibma <62669274+Siroibma@users.noreply.github.com>
2021-05-24 17:18:13 -0400

    Created Multiple Functions
    Added create,update,delete and add user

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjgwLCJleHAiOjE2MjIxMzI0ODB9.GEoBhrlckoVTjt4upuwyzNdD2G5ZJ3KK4ZxGCPeG-PM"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjkwLCJleHAiOjE2MjI0NzYwNTZ9.8jjzHC31-CDYiZH-bd0wYsKItFt8ZjiPFMRWevwL6Is"
 var amount = 5;
 
 console.log(token);
@@ -22,19 +22,17 @@ let app = new Vue({
         console.log(this.results)
       })
   },
-  methods: {
-    formSubmit(e) {
-        e.preventDefault();
-        let currentObj = this;
-        this.axios.post('http://206.189.202.188:2523/api/users/add', {
-            email: 'ambioris42432@gmail.com',
-            password: 'test'
-        })
-    }
-},
 });
 
-function addUser() {
+function addUser(event) {
+
+  console.log("hello");
+
+  event.preventDefault();
+
+
+  let email = "amber123@gmail.com";
+  let password = "123";
 
   const config = {
     headers: {
@@ -44,25 +42,28 @@ function addUser() {
 
   axios
     .post('http://206.189.202.188:2523/api/users/add', {
-      email: 'ambioris42432@gmail.com',
-      password: 'test'
+      email: `${email}`,
+      password: `${password}`
     },
     config
     )
-    .then(res => console.log(res.request.response.data))
+    .then(res => 
+      console.log(res.request.response)
+    )
+    .then()
     .catch(err => console.error(err));
 }
 
-function deleteArticle() {
+function deleteArticle(event) {
+  event.preventDefault();
 
-  /*Article_ID = String(document.getElementById('AID').value);
+  let Article_ID = String(document.getElementById('article_ID').value);
 
-  Article_url = "http://206.189.202.188:2523/api/articles/delete/";
+  let Article_url = "http://206.189.202.188:2523/api/articles/delete/";
 
   console.log(Article_ID);
 
-  delete_url = Article_url.concat(Article_ID);
-  */
+  let delete_url = Article_url.concat(Article_ID);
 
   const config = {
     headers: {
@@ -72,23 +73,28 @@ function deleteArticle() {
   };
 
 
-  //console.log(delete_url);
+  console.log(delete_url);
 
 
   axios
     .delete(
-      'http://206.189.202.188:2523/api/articles/delete/8',
-      {
-        id: '8'
-      },
+      `${delete_url}`,
       config
     )
     .then(result => console.log(result))
     .catch(err => console.error(err));
+
+    console.log(result);
 }
 
 
-function createArticle() {
+function createArticle(event) {
+
+  event.preventDefault();
+
+  let Article_Title = String(document.getElementById('title').value);
+  let Article_Body = String(document.getElementById('body').value);
+  let Article_Category = String(document.getElementById('category_id').value);
 
 
   const config = {
@@ -102,9 +108,50 @@ function createArticle() {
     .post(
       'http://206.189.202.188:2523/api/articles/add',
       {
-        title: 'New Todo',
-        body: 'hello',
-        category_id: '0'
+        title: `${Article_Title}`,
+        body: `${Article_Body}`,
+        category_id: `${Article_Category}`
+      },
+      config
+    )
+    .then(result => console.log(result))
+    .catch(err => console.error(err));
+
+    console.log(result);
+}
+
+function updateArticle(event) {
+
+  event.preventDefault();
+
+  let Article_Title = String(document.getElementById('update_title').value);
+  let Article_Body = String(document.getElementById('update_body').value);
+  let Article_ID = String(document.getElementById('AID').value);
+
+
+
+  let Article_url = "http://206.189.202.188:2523/api/articles/edit/";
+
+  console.log(Article_ID);
+
+  let edit_url = Article_url.concat(Article_ID);
+
+
+
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}` 
+    }
+  };
+
+  axios
+    .post(
+      `${edit_url}`,
+      {
+        title: `${Article_Title}`,
+        body: `${Article_Body}`,
       },
       config
     )
@@ -117,6 +164,7 @@ function createArticle() {
 
 
 
-document.getElementById('post').addEventListener('click', addUser);
 document.getElementById('create').addEventListener('click', createArticle);
 document.getElementById('delete').addEventListener('click', deleteArticle);
+document.getElementById('register').addEventListener('click', addUser);
+document.getElementById('update').addEventListener('click', updateArticle);
